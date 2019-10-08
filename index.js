e7bd11acdd08fa4ef10a2009bf9491a56a5c1ccd
Trevor <tgrabham@sfu.ca>
2019-10-08 00:36:55 -0700

    changed home page

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var dbRows = null;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/tokimon.ejs'))
+  .get('/', (req, res) => res.render('pages/tokimon.ejs', dbRows))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
