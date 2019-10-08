16bf724d73a9860fd4dc0ddf72c6ad802c72b0c3
Trevor <tgrabham@sfu.ca>
2019-10-08 00:38:10 -0700

    changed home page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-var dbRows = null;
+var db = {dbRows: null}
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
