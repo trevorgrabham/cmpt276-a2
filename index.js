6abdae0181315a960cb711d3793031a655eab416
Trevor Grabham <tgrabham@sfu.ca>
2019-10-09 18:23:46 -0700

    hopefully the bars work properly for display and the link for home page

--- index.js
+++ index.js
@@ -8,4 +8,18 @@ const app = express();
   app.set('view engine', 'ejs');
   app.get('/', (req, res) => res.render('pages/tokimon'));
   app.get('/newTokimon', (req, res) => res.render('pages/newTokimon'));
+  app.post('/display', (req, res) => {
+    var results = {name: "test"
+      weight: 0
+      height: 0
+      fly: 0
+      fight: 0
+      fire: 0
+      water: 0
+      electric: 0
+      ice: 0
+      total: 0
+    }
+    res.render('pages/displayTokimon', results);
+  });
   app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
