7365b236b9b636cd864534c84070ff8491d35db2
Trevor Grabham <tgrabham@sfu.ca>
2019-10-09 23:53:14 -0700

    updated the homepage to display the database

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
+const { Pool } = require('pg');
+var pool = new Pool({connectionString: process.env.DATABASE_URL});
+
+
 const app = express();
   app.use(express.static(path.join(__dirname, 'public')));
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'ejs');
-  app.get('/', (req, res) => res.render('pages/tokimon'));
+  app.get('/', (req, res) => {
+    var res = pool.query("SELECT * FROM tokimon");
+    if(res != null){
+      var resRows = {'rows': res.rows};
+    }
+    res.render('pages/tokimon', resRows);
+  });
   app.get('/newTokimon', (req, res) => res.render('pages/newTokimon'));
   app.post('/display/:name', (req, res) => {
     var results = {name: name
@@ -24,7 +34,7 @@ const app = express();
   });
   app.get('/add', (req, res) => res.render('pages/newTokimon'));
   app.post('/search', (req, res) => {
-    var query = `SELECT * FROM tokimon WHERE NAME=${req.body.search}`;
+    var query = pool.query(`SELECT * FROM tokimon WHERE NAME=${req.body.search}`);
     if(query == null){
         // send error message
     }  else{
