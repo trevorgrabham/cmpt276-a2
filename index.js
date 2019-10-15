d4d6c0b1afdfe3484306b30b4ebffc65a1b07ba2
Trevor <tgrabham@sfu.ca>
2019-10-15 00:02:14 -0700

    trying to get the insert working

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var pool = new Pool({connectionString: process.env.DATABASE_URL, ssl: true});
 
 const app = express();
   app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false}));
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'ejs');
   app.get('/', async (req, res) => {
