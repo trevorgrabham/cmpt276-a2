823c61eceadc1ccb6fd412410fef8368072c5540
Trevor <tgrabham@sfu.ca>
2019-10-15 02:33:23 -0700

    creating a new app

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ const app = express();
     try{
       const client = await pool.connect();
       var test = await client.query(`select * from tokimon where name='${name}'`);
-      if(test.rows != null){
+      if(test){
         console.log(`updating\n ${test} \n that was tests value`);
         client.query(`update tokimon set weight=${weight},height=${height},fly=${fly},fight=${fight},fire=${fire},water=${water},electric=${electric},ice=${ice},total=${total} where name='${name}'`);
       } else {
